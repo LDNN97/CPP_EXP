a23f551349c8d62fa7a7f5616864acd9b84576d5
LDNN97 <ldnn97@foxmail.com>
2020-04-01 23:14:56 +0800

    Some Experiments
     Changes to be committed:
    	modified:   CPP11/main.cpp
    	modified:   Taskflow/main.cpp

--- CPP11/main.cpp
+++ CPP11/main.cpp
@@ -1,42 +1,70 @@
-#include <iostream>
-#include <vector>
-#include <numeric>      // std::iota
-#include <algorithm>    // std::sort, std::stable_sort
-#include <fstream>
-
-using namespace std;
-
-template <typename T>
-
-vector<size_t> sort_indexes(const vector<T> &v) {
-    vector<size_t> idx(v.size());
-    iota(idx.begin(), idx.end(), 0);
-    stable_sort(idx.begin(), idx.end(), [&v](size_t i1, size_t i2) {return v[i1] < v[i2];});
-    return idx;
-}
-
-constexpr int fibonacci(const int n) {
-    return  n == 1 || n == 2 ? 1 : fibonacci(n-1) + fibonacci(n-2);
-}
-
-int main() {
-//    vector<int> v(30);
-//    v = {-11, -11, -11, -11, -11, -11,-11, -11, -11, -11, -11, -11,-11, -11, -11, -11, -11, -11,-11, -11, -11, -11, -11, -1,-11, -11, -11, -11, -11, -11};
-//    for (auto i: sort_indexes(v)) {
-//        cout << i << " " << v[i] << endl;
-//    }
+//#include <iostream>
+//#include <vector>
+//#include <numeric>      // std::iota
+//#include <algorithm>    // std::sort, std::stable_sort
+//#include <fstream>
 //
-//    for (int i = 0; i < 10; i++) {
-//        string file = "test/Aster.txt";
-//        char num = static_cast<char>(i+'0');
-//        cout << num << endl;
-//        file.insert(10, 1, num);
-//        ofstream fil(file);
-//        fil << i << endl;
-//        fil.close();
-//    }
-//    int len = 10;
-//    char str[len];
-    cout << fibonacci(10) << endl;
+//using namespace std;
+//
+//template <typename T>
+//
+//vector<size_t> sort_indexes(const vector<T> &v) {
+//    vector<size_t> idx(v.size());
+//    iota(idx.begin(), idx.end(), 0);
+//    stable_sort(idx.begin(), idx.end(), [&v](size_t i1, size_t i2) {return v[i1] < v[i2];});
+//    return idx;
+//}
+//
+//constexpr int fibonacci(const int n) {
+//    return  n == 1 || n == 2 ? 1 : fibonacci(n-1) + fibonacci(n-2);
+//}
+//
+//int main() {
+////    vector<int> v(30);
+////    v = {-11, -11, -11, -11, -11, -11,-11, -11, -11, -11, -11, -11,-11, -11, -11, -11, -11, -11,-11, -11, -11, -11, -11, -1,-11, -11, -11, -11, -11, -11};
+////    for (auto i: sort_indexes(v)) {
+////        cout << i << " " << v[i] << endl;
+////    }
+////
+////    for (int i = 0; i < 10; i++) {
+////        string file = "test/Aster.txt";
+////        char num = static_cast<char>(i+'0');
+////        cout << num << endl;
+////        file.insert(10, 1, num);
+////        ofstream fil(file);
+////        fil << i << endl;
+////        fil.close();
+////    }
+////    int len = 10;
+////    char str[len];
+//    cout << fibonacci(10) << endl;
+//
+//}
 
+#include <iostream>
+#include <string>
+
+void reference(std::string& str) {
+    std::cout << "左值" << std::endl;
+}
+void reference(std::string&& str) {
+    std::cout << "右值" << std::endl;
+}
+
+int main()
+{
+    std::string lv1 = "string,"; // lv1 是一个左值
+    // std::string&& r1 = lv1; // 非法, 右值引用不能引用左值
+    std::string&& rv1 = std::move(lv1); // 合法, std::move可以将左值转移为右值
+    std::cout << rv1 << std::endl; // string,
+
+    const std::string& lv2 = lv1 + lv1; // 合法, 常量左值引用能够延长临时变量的生命周期
+    // lv2 += "Test"; // 非法, 常量引用无法被修改
+    std::cout << lv2 << std::endl; // string,string
+
+    std::string&& lv3 = lv1 + lv2; // 合法, 右值引用延长临时对象生命周期
+
+    reference(lv1 + lv2); // 输出左值
+
+    return 0;
 }
--- Taskflow/main.cpp
+++ Taskflow/main.cpp
@@ -1,4 +1,11 @@
+#include <fstream>
 #include "taskflow/taskflow.hpp"
+
+int evaluate(int x) {
+    for (int i = 0;  i < 1000000; i++)
+        x++;
+    return x;
+}
 
 int main(){
 
@@ -7,32 +14,39 @@ int main(){
     // Create a taskflow of eight tasks
     tf::Taskflow taskflow;
 
-    auto A = taskflow.emplace([] () { std::cout << "1\n"; });
-    auto B = taskflow.emplace([] () { std::cout << "2\n"; });
-    auto C = taskflow.emplace([] () { std::cout << "3\n"; });
-    auto D = taskflow.emplace([] () { std::cout << "4\n"; });
-    auto E = taskflow.emplace([] () { std::cout << "5\n"; });
-    auto F = taskflow.emplace([] () { std::cout << "6\n"; });
-    auto G = taskflow.emplace([] () { std::cout << "7\n"; });
-    auto H = taskflow.emplace([] () { std::cout << "8\n"; });
-
+    int nn[8] {1, 2, 3, 4, 5, 6, 7, 8};
+    int ans[8] {};
+    int anss;
+    auto A = taskflow.emplace([&nn, &ans] () { ans[0] = evaluate(nn[0]); });
+    auto B = taskflow.emplace([&nn, &ans] () { ans[1] = evaluate(nn[1]); });
+    auto C = taskflow.emplace([&nn, &ans] () { ans[2] = evaluate(nn[2]); });
+    auto D = taskflow.emplace([&nn, &ans] () { ans[3] = evaluate(nn[3]); });
+    auto A1 = taskflow.emplace([&nn, &ans] () { ans[4] = evaluate(nn[4]); });
+    auto B1 = taskflow.emplace([&nn, &ans] () { ans[5] = evaluate(nn[5]); });
+    auto C1 = taskflow.emplace([&nn, &ans] () { ans[6] = evaluate(nn[6]); });
+    auto D1 = taskflow.emplace([&nn, &ans] () { ans[7] = evaluate(nn[7]); });
     A.name("A");
     B.name("B");
     C.name("C");
     D.name("D");
-    E.name("E");
-    F.name("F");
-    G.name("G");
-    H.name("H");
+    A1.name("A1");
+    B1.name("B1");
+    C1.name("C1");
+    D1.name("D1");
+
 
     // create a default observer
     auto observer = executor.make_observer<tf::ExecutorObserver>();
 
     // run the taskflow
-    executor.run(taskflow).get();
+    auto result = executor.run(taskflow);
+    result.get();
+
+    for (int i = 0; i < 8; i++)
+        std::cout << i << " " << ans[i] << std::endl;
 
     // dump the execution timeline to json (view at chrome://tracing)
-    observer->dump(std::cout);
-
+    std::ofstream ofs("timestamps.json");
+    observer->dump(ofs);
     return 0;
 }
